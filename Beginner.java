59668e0c5f08f572a1717ae2b394d4e824fcf9b7
CodeCode1212 <sonngunguoi3@gmail.com>
2024-10-15 19:27:35 +0900

    Update Beginner.java

--- Beginner.java
+++ Beginner.java
@@ -3,20 +3,20 @@ import java.util.Random;
 import java.util.Scanner;
 public class Beginner {
   public static void main(String[] args) {
+  Scanner info = new Scanner(System.in); 
   System.out.println("ようこそ占いの館へ");
   System.out.println("あなたの名前を入力してください＞");
-  Scanner info = new Scanner(System.in);
-  String name = info.nextLine();
+   String name = info.nextLine();
   System.out.println("あなたの年齢を入力してください＞");
-  int age = info.nextInt();
+   int age = info.nextInt();
   String[] fortune = {"大吉", "中吉", "吉", "凶"};
   Random luck = new Random();
-  int a = luck.nextInt(3);
+   int a = luck.nextInt(3);
   System.out.print("占いの結果が出ました！ \n" + age + "の" + name + "さん、あなたの運命は" + fortune[a] + "です。" );
   
   
 
-   info.close(); // your code
+   info.close(); 
     }
 
 }
